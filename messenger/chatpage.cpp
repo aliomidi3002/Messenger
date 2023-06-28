6eee8f22d93ef6edae4c62ec0ffc9c6ba112af73
imzzzzzz <izamanimoghaddam@gmail.com>
2023-06-28 06:13:07 -0700

    skjlfh

--- messenger/chatpage.cpp
+++ messenger/chatpage.cpp
@@ -7,9 +7,7 @@
 #include <QKeyEvent>
 #include <QWidget>
 #include <QScrollArea>
-
-
-QString extractSubstring(const QString& original, QString& first, QString& second) {
+QString extractSubstring_for_extracting_the_chat_info(const QString& original, QString& first, QString& second) {
     int firstPos = original.indexOf(first);
     if (firstPos == -1) {
         // First string not found
@@ -160,6 +158,189 @@ QString sendmessagechannel_chat_to_server(QString token,QString dst , QString bo
     QString response_code_login_server = response_code(response);
 
     return response_code_login_server;
+}
+struct MessageBlock {
+    QString src;
+    QString dst;
+    QString body;
+    QString date;
+};
+QString findSubstringAndReturnRest(const QString& originalString, const QString& substring)
+{
+    int index = originalString.indexOf(substring);
+
+    if (index != -1) {
+        // Found the substring, return the portion of the string starting from the occurrence
+        return originalString.mid(index + substring.length());
+    }
+
+    // Substring not found, return an empty string
+    return "";
+}
+
+QString getStringBetweenLastTwoStrings(const QString& first, const QString& second, const QString& third)
+{
+    int secondIndex = first.lastIndexOf(second);
+    int thirdIndex = first.lastIndexOf(third);
+
+    if (secondIndex == -1 || thirdIndex == -1 || secondIndex >= thirdIndex)
+    {
+        return QString(); // Return an empty string if either of the substrings is not found or the order is incorrect.
+    }
+
+    int startIndex = secondIndex + second.length();
+    int length = thirdIndex - startIndex;
+    return first.mid(startIndex, length);
+}
+MessageBlock* getuserchats_server_to_chat_display(QString token,QString dst) {
+    QString url1= "http://api.barafardayebehtar.ml:8080/getuserchats?token=";
+    QString url2= "&dst=";
+    url1=url1+token+url2+dst;
+    QUrl url(url1);
+    /*
+    QUrl url2("&password=");
+    url = url.resolved(user1);
+    url = url.resolved(url2);
+    url = url.resolved(pass1);*/
+    // Create a QNetworkAccessManager object
+    QNetworkAccessManager manager;
+
+    // Create a QNetworkRequest object and set the URL
+    QNetworkRequest request;
+    request.setUrl(url);
+
+    // Send the GET request
+    QNetworkReply* reply = manager.get(request);
+
+    // Wait for the request to finish
+    QEventLoop loop;
+    QObject::connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    loop.exec();
+
+    QString response;
+    if (reply->error() == QNetworkReply::NoError) {
+        // Reading the response data
+        QByteArray responseData = reply->readAll();
+        response = QString(responseData);
+    } else {
+        // Handle error cases
+        qDebug() << "Error:" << reply->errorString();
+    }
+
+    // Clean up
+    reply->deleteLater();
+
+    QString first="There Are -";
+    QString second= "- Messages\"";
+    QString temp=response;
+    int num=getStringBetweenLastTwoStrings(temp,first,second).toInt(),i=0;
+    //qDebug()<<num<<"\n\n";
+    MessageBlock* answer_to_the_chat_info = new MessageBlock[num];
+    //qDebug() << response<<"\n\n\n";
+    QString serverResponse = response;
+
+    // Parse the JSON response
+    QJsonDocument jsonResponse = QJsonDocument::fromJson(serverResponse.toUtf8());
+    if (!jsonResponse.isNull() && jsonResponse.isObject()) {
+        QJsonObject rootObject = jsonResponse.object();
+
+        // Iterate over each block
+        for (const QString& key : rootObject.keys()) {
+            if (key.startsWith("block ")) {
+                QJsonObject blockObject = rootObject.value(key).toObject();
+
+                // Extract values and store in struct
+
+                answer_to_the_chat_info[i].src = blockObject.value("src").toString();
+                answer_to_the_chat_info[i].dst = blockObject.value("dst").toString();
+                answer_to_the_chat_info[i].body = blockObject.value("body").toString();
+                answer_to_the_chat_info[i].date = blockObject.value("date").toString();
+
+                // Print the values for verification
+                //qDebug() << "Block" << key;
+                //qDebug() << "Source:" << answer_to_the_chat_info[i].src;
+                //qDebug() << "Destination:" << answer_to_the_chat_info[i].dst;
+                //qDebug() << "Body:" << answer_to_the_chat_info[i].body;
+                //qDebug() << "Date:" << answer_to_the_chat_info[i].date;
+                i++;
+            }
+        }
+    }
+
+    //qDebug() <<"\n\n\n"<< response;
+    //qDebug() <<"238476874682374\tefwwwe\twr";
+    return answer_to_the_chat_info;
+
+
+
+    //extractJsonValue(responseData,"bo")
+
+}
+
+QString response_code(QString Server_Response){
+    //seperating the code out of the respose of the server
+    QString searchString1 = "\"204\"";
+    QString searchString2 = "\"404\"";
+    QString searchString3 = "\"401\"";
+    QString searchString4 = "\"200\"";
+    if(Server_Response.contains(searchString1)){
+        return "204";
+    }
+    if(Server_Response.contains(searchString2)){
+        return "404";
+    }
+    if(Server_Response.contains(searchString3)){
+        return "401";
+    }
+    if(Server_Response.contains(searchString4)){
+        return "200";
+    }
+    else{
+        return "Error";
+    }
+
+}
+QString signup(QString user,QString pass) {
+
+    QString url1= "http://api.barafardayebehtar.ml:8080/signup?username=";
+    QString url2= "&password=";
+
+    url1=url1+user+url2+pass;
+    QUrl url(url1);
+    /*
+    QUrl url2("&password=");
+    url = url.resolved(user1);
+    url = url.resolved(url2);
+    url = url.resolved(pass1);*/
+    // Create a QNetworkAccessManager object
+    QNetworkAccessManager manager;
+
+    // Create a QNetworkRequest object and set the URL
+    QNetworkRequest request;
+    request.setUrl(url);
+
+    // Send the GET request
+    QNetworkReply* reply = manager.get(request);
+
+    // Wait for the request to finish
+    QEventLoop loop;
+    QObject::connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    loop.exec();
+
+    QString response;
+    if (reply->error() == QNetworkReply::NoError) {
+        // Reading the response data
+        QByteArray responseData = reply->readAll();
+        response = QString(responseData);
+    } else {
+        // Handle error cases
+        //qDebug() << "Error:" << reply->errorString();
+    }
+
+    // Clean up
+    reply->deleteLater();
+
+    return response_code(response);
 }
 
 Chatpage::Chatpage(QWidget *parent, const userID& currentUser) :
